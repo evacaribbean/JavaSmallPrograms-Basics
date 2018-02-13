985167d0456deaf15fb09ccc6c3dd68dbcb060f2
cabbitest <evacaribbeansea@gmail.com>
2018-02-13 16:20:39 +0100

    update

--- bp09WindowWithControls2FX.java
+++ bp09WindowWithControls2FX.java
@@ -13,28 +13,28 @@ public class bp09WindowWithControls2FX extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
-        Button b1 = new Button("Button 1");
+        Button b1 = new Button("...a long button with some text attached");
         Button b2 = new Button("Button 1");
-        Button b3 = new Button("Button 1");
-        Button b4 = new Button("Button 1");
+        Button b3 = new Button("Button 2");
+        Button b4 = new Button("Button 3");
 
-        Label la1 = new Label("text");
-        Label la2 = new Label("text");
+        Label la1 = new Label("Below follow Button 1 and Button 2");
+        Label la2 = new Label("Below follows Button 3");
 
-        TextField t1 = new TextField("text");
+        TextField t1 = new TextField("...here is a text field and...");
 
         Group group = new Group();
         group.getChildren().addAll(b1, b2, b3, b4, la1, la2, t1);
 
-        b1.setLayoutX(20); b1.setLayoutY(20);
-        b2.setLayoutX(20); b2.setLayoutY(60);
-        b3.setLayoutX(20); b3.setLayoutY(100);
-        b4.setLayoutX(20); b4.setLayoutY(140);
+        t1.setLayoutX(20); t1.setLayoutY(20);
+        b1.setLayoutX(20); b1.setLayoutY(60);
 
-        la1.setLayoutX(20); la1.setLayoutY(170);
-        la2.setLayoutX(20); la2.setLayoutY(210);
+        la1.setLayoutX(20); la1.setLayoutY(140);
+        b2.setLayoutX(20); b2.setLayoutY(165);
+        b3.setLayoutX(20); b3.setLayoutY(195);
 
-        t1.setLayoutX(20); t1.setLayoutY(240);
+        la2.setLayoutX(20); la2.setLayoutY(240);
+        b4.setLayoutX(20); b4.setLayoutY(265);
 
         Scene scene = new Scene(group, 700, 500);
         stage.setScene(scene);

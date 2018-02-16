b01094b4dcd58d6e30de9d235b36af0a585bae81
cabbitest <evacaribbeansea@gmail.com>
2018-02-16 13:31:02 +0100

    update

--- bp06WindowWithControls1FX.java
+++ bp06WindowWithControls1FX.java
@@ -1,9 +1,9 @@
 
 import javafx.application.Application;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.RadioButton;
-import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 
@@ -19,7 +19,11 @@ public class bp06WindowWithControls1FX extends Application {
                                    new Label(" This is a label to Button B "),
                                    new Button("Button B"),
                                    new Label(" This is a label to the Radio button "),
-                                   new RadioButton("A Radio button"));
+                                   new RadioButton("A Radio button"),
+                                   new Label("\nIn the next the file \"bp07WindowWithPanesFX.java\" you" +
+                                           " will find even more controls.\nBut here you can also test" +
+                                           " to see what happens when you change panes. In the code\ncomments" +
+                                           " to that file your will find all the JavaFX's predefined panes."));
 
         Scene scene = new Scene(fPane, 700, 500);
         stage.setScene(scene);
@@ -34,20 +38,22 @@ public class bp06WindowWithControls1FX extends Application {
 
 /*
 
-¤  In this file will the elements (controls) show "in a flow" as it is "FlowPane" that is
-the chosen pane. That means that the controls will place themselves in a row and just
-continue on the next row, when the have reached the end of the window.
+¤  In this file will the elements (controls) show "in a flow" (FlowPane).
+Every control will simply just place itself next to the previous one. And
+when the row is full of controls, continue on the next row.
 
-You can easily check this by resize the window, when you run the program.
-
-
-¤  File "bp09WindowWithControls2FX.java" will show one effective way on how to instead coding,
-giving the controls a static location. This is done by the pane "Group" (or more correct
-the "layout bound": Group).
-
-Here you have the oppertunity to give each and every control an exact position.
+When running the program, you can easily check this by resize the window.
 
 
-A general link on FX controls  ->  https://docs.oracle.com/javafx/2/ui_controls/overview.htm
+¤  File "bp08WindowWithGrid1FX.java" will show a more effective way on how
+to coding, giving the controls a static location. This is done by the pane:
+"Group" (or more correct the "layout bound": Group).
+
+Here you will have the oppertunity to give each and every control an exact
+position.
+
+
+A general link on FX controls:
+->  https://docs.oracle.com/javafx/2/ui_controls/overview.htm
 
 */
